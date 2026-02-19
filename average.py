2ef9bf638322d146a6babd23036b2e6e4f026be4
Sudeep-official48 <sudeepofficial46@gmail.com>
2026-02-19 19:51:51 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,6 @@
+# Find an average of two numbers entered bt the user 
+
+a = int(input("Enter the number 1"))
+b = int(input("Enter the number 2"))
+
+print("The average of these two number is", (a+b)/2)
