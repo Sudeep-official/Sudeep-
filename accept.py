4b3bc3f5b27ce5fa4a6d701568d3ed813c05f573
Sudeep-official48 <sudeepofficial46@gmail.com>
2026-02-20 21:05:28 +0530

    Add files via upload

---
+++ accept.py
@@ -0,0 +1,18 @@
+# Program to accept marks of 6 subjects and display them in a sort manner 
+Marks = []
+s1 = int(input("Enter the Marks here  : "))
+Marks.append(s1)
+s2 = int(input("Enter the Marks here  : "))
+Marks.append(s2)
+s3 = int(input("Enter the Marks here  : "))
+Marks.append(s3)
+s4 = int(input("Enter the Marks here  : "))
+Marks.append(s4)
+s5 = int(input("Enter the Marks here  : "))
+Marks.append(s5)
+s6 = int(input("Enter the Marks here  : "))
+Marks.append(s6)
+
+Marks.sort()
+
+print(Marks)
