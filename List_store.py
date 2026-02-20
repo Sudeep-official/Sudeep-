e6e0876f85e49ff00c5da2841f42af6582b56bfb
Sudeep-official48 <sudeepofficial46@gmail.com>
2026-02-20 20:53:22 +0530

    Add files via upload

---
+++ List_store.py
@@ -0,0 +1,19 @@
+# program to store different seven string in a list entered by user 
+
+string = []
+s1 = input("Enter the name: ")
+string.append(s1)
+s2 = input("Enter the name: ")
+string.append(s2)
+s3 = input("Enter the name: ")
+string.append(s3)
+s4 = input("Enter the name: ")
+string.append(s4)
+s5 = input("Enter the name: ")
+string.append(s5)
+s6 = input("Enter the name: ")
+string.append(s6)
+s7 = input("Enter the name: ")
+string.append(s7)
+
+print(string)
